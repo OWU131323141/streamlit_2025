e1385ea7df8ac9a95252d5b9e7f2ce6f791cfd8e
OWU131323141 <s1323141@cst.otsuma.ac.jp>
2025-06-13 02:45:19 +0900

    Update sima.py

--- sima/sima.py
+++ sima/sima.py
@@ -14,7 +14,7 @@ st.markdown(
 st.header("旅行先におすすめの島診断🗾")
 
 st.markdown('<p style="font-size:15px; font-weight:bold;">どの季節に旅行したいですか？</p>', unsafe_allow_html=True)
-kisetu = st.radio(["春🌸","夏🌳","秋🍂","冬⛄"])
+kisetu = st.radio["春🌸","夏🌳","秋🍂","冬⛄"]
 print(kisetu)
 
 kotoba = st.radio("この中で一番惹かれる言葉は？",["神秘的✨","のんびり🐢","ワイルド💪"])
