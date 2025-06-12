3acdcd5aa2326fe72ba540b32a00032f980b75f5
OWU131323141 <s1323141@cst.otsuma.ac.jp>
2025-06-13 02:52:33 +0900

    Update sima.py

--- sima/sima.py
+++ sima/sima.py
@@ -13,15 +13,15 @@ st.markdown(
 
 st.header("旅行先におすすめの島診断🗾")
 
-st.markdown('<br><p style="font-size:18px; font-weight:bold;">どの季節に旅行したいですか？</p>', unsafe_allow_html=True)
+st.markdown('<br><p style="font-size:20px; font-weight:bold;">どの季節に旅行したいですか？</p>', unsafe_allow_html=True)
 kisetu = st.radio("この中から一つ選んでください",["春🌸","夏🌳","秋🍂","冬⛄"])
 print(kisetu)
 
-st.markdown('<p style="font-size:18px; font-weight:bold;">この中で一番惹かれる言葉は？</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size:20px; font-weight:bold;">この中で一番惹かれる言葉は？</p>', unsafe_allow_html=True)
 kotoba = st.radio("この中から一つ選んでください",["神秘的✨","のんびり🐢","ワイルド💪"])
 print(kotoba)
 
-st.markdown('<p style="font-size:18px; font-weight:bold;">この中で１つだけ持っていけるとしたら？</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size:20px; font-weight:bold;">この中で１つだけ持っていけるとしたら？</p>', unsafe_allow_html=True)
 motimono = st.radio("この中から一つ選んでください",["カメラ📸","水着👙","スニーカー👟"])
 print(motimono)
 
